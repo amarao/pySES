8eb871ccc0aa42d2ec8017641ef6c5f33ddba018
George Shuklin <george.shuklin@gmail.com>
2015-06-29 11:53:20 -0400

    Disk class, begin refactoring

--- pySES/disk.py
+++ pySES/disk.py
@@ -1,4 +1,37 @@
-class Disk(dict):
+import os
+class Disk(object):
+    model = None
+    sas_address = None
+    sas_device_handle = None
+    timeout = None
+    eh_timeout = None
+    sg = None
+
+
+    def _readattr(self, path, attr):
+        try:
+            return file(os.path.join(path, attr), 'r').read().strip()
+        except None:
+            return None
+
+
+    def _getsubdir(self, path, subpath):
+        try:
+            return os.listdir(os.path.join(path, subpath))[0]
+        except:
+            return None
+
+
     def __init__(self, path):
-        dict.__init__(self)
-        self['path'] = path
+        self.path = path
+        self.name = os.listdir(os.path.join(path, 'block'))[0]
+#todo sector_size from queue       self['size'] = self._readattr(os.path.join('block', self['name'], "size"))
+        self.model = self._readattr(self.path, 'model')
+        self.sas_address = self._readattr(self.path, 'sas_address')
+        self.sas_device_handle = self._readattr(self.path, 'sas_device_handle')
+        self.timeout = self._readattr(self.path, 'timeout')
+        self.eh_timeout = self._readattr(self.path, 'eh_timeout')
+        self.sg = os.listdir(os.path.join(path, 'scsi_generic'))[0]
+
+    def __str__(self):
+        return ' '.join([self.name, self.model])
--- pySES/enclosure.py
+++ pySES/enclosure.py
@@ -1,66 +1,63 @@
 #!/usr/bin/python
 import os
 from .disk import Disk
+#from .slot import Slot
 class EnclosureException(Exception):
     pass
 
-class Enclosure(dict):
+class Enclosure(object):
     _skip_list = ['device', 'power', 'subsystem']
-    slot_list = []
-    def _is_enclosure(self):
+    slots = []
+    def _is_enclosure(self, path):
         '''
             check if enclosure is really an enclosure
         '''
-        return os.path.isdir(os.path.join(self['path'], 'device', 'enclosure'))
+        return os.path.isdir(os.path.join(path, 'device', 'enclosure'))
 
 
-    def _devread(self,name):
-        fullpath = os.path.join(self['path'], 'device', name)
+    def _devread(self,path, name):
+        fullpath = os.path.join(path , 'device', name)
         return file(fullpath, 'r').read().strip()
 
 
-    def _slotread(self,slot,name):
-        fullpath = os.path.join(self['path'], slot, name)
+    def _slotread(self, path, slot, name):
+        fullpath = os.path.join(path, slot, name)
         return file(fullpath, 'r').read().strip()
 
     def _fill_enclosure_info(self):
-        self['name'] = os.path.split(self['path'])[-1]
-        self['vendor'] = self._devread('vendor')
-        self['model'] = self._devread('model')
-        self['eh_timeout'] = self._devread('eh_timeout')
-        self['sas_address'] = self._devread('sas_address')
-        self['rev'] = self._devread('rev')
-        self['sas_device_handle'] = self._devread('sas_device_handle')
-        self['sg'] = os.listdir(os.path.join(self['path'],'device/scsi_generic'))[0]
+        self.name = os.path.split(self.path)[-1]
+        self.vendor = self._devread(self.path, 'vendor')
+        self.model = self._devread(self.path, 'model')
+        self.eh_timeout = self._devread(self.path, 'eh_timeout')
+        self.sas_address = self._devread(self.path, 'sas_address')
+        self.sas_device_handle = self._devread(self.path, 'sas_device_handle')
+        self.sg = os.listdir(os.path.join(self.path, 'device/scsi_generic'))[0]
 
-    def get_slot_info(self, slot):
+    def _get_slot_info(self, slot):
         dev = {'slot': slot}
-        dev['active'] = self._slotread(slot, 'active')
-        dev['fault'] = self._slotread(slot, 'fault')
-        dev['locate'] = self._slotread(slot, 'locate')
-        dev['disk'] = Disk(os.path.join(self['path'], slot, 'device'))
+        dev['active'] = self._slotread(self.path, slot, 'active')
+        dev['fault'] = self._slotread(self.path, slot, 'fault')
+        dev['locate'] = self._slotread(self.path, slot, 'locate')
+        dev['disk'] = Disk(os.path.join(self.path, slot, 'device'))
         return dev
 
     def __init__(self, path):
-        dict.__init__(self)
-        self['path'] = path
-        if not self._is_enclosure():
+        if not self._is_enclosure(path):
             raise EnclosureException("Not an enclosure")
+        self.path = path
         self._fill_enclosure_info()
-        slots = []
         for el in os.listdir(path):
             if el in self._skip_list:
                 continue
-            if not os.path.isdir(os.path.join(self['path'], el)):
+            if not os.path.isdir(os.path.join(self.path, el)):
                 continue
-            slots.append(self.get_slot_info(el))
-        self['slots'] = slots
+            self.slots.append(self._get_slot_info(el))
 
     def __str__(self):
         return "Enclosure %s %s %s (%s slots)" % (
-            self["name"],
-            self["model"],
-            self["vendor"],
-            len(self["slots"])
+            self.name,
+            self.model,
+            self.vendor,
+            len(self.slots)
         )
 
